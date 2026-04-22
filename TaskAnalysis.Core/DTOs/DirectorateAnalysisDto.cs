d729e6234ff0bb5c789db2884c55f1f0bd962c63
AbdullahEmirOner <abdullahemironer@gmail.com>
2026-04-22 17:24:28 +0300

    Remove DepartmentAnalysisDto and DirectorateAnalysisDto classes
    Deleted DepartmentAnalysisDto and DirectorateAnalysisDto classes from their respective files, removing related properties and DTO definitions.

--- TaskAnalysis.Core/DTOs/DirectorateAnalysisDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TaskAnalysis.Service;
-
-// DTO'lar
-public class DirectorateAnalysisDto
-{
-    public string DirektorlukAnalizi { get; set; }
-    public List<DepartmentAnalysisDto> Mudurlukler { get; set; }
-}
